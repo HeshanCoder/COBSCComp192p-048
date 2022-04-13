a741d947ffd66626eb499f07146739d5baef7fea
HeshanCoder <heshancoder@gmail.com>
2022-04-13 00:57:32 -0700

    initial commit

---
+++ nibm_broker/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  nibm_broker
+//
+//  Created by HeshanHH on 4/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("NIBM_Brokes")
+            .padding()
+        
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 11")
+    }
+}
---
+++ nibm_broker/nibm_brokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  nibm_brokerApp.swift
+//  nibm_broker
+//
+//  Created by HeshanHH on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct nibm_brokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
